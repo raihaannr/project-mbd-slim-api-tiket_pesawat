7555143081a98ed76d36da4c094b8603dfa1c1fd
Raihan Ramadhani <11211069@student.itk.ac.id>
2023-10-23 11:50:44 +0800

    get tiap tabel

--- app/routes.php
+++ app/routes.php
@@ -10,5 +10,47 @@ use Slim\App;
 use Slim\Interfaces\RouteCollectorProxyInterface as Group;
 
 return function (App $app) {
-    
+    // get tabel kelas
+    $app->get('/kelas', function(Request $request, Response $response) {
+        $db = $this->get(PDO::class);
+
+        $query = $db->query('CALL GetKelas()');
+        $results = $query->fetchAll(PDO::FETCH_ASSOC);
+        $response->getBody()->write(json_encode($results));
+
+        return $response->withHeader("Content-Type", "application/json");
+    });
+
+    // get tabel pemesanan
+    $app->get('/pemesanan', function(Request $request, Response $response) {
+        $db = $this->get(PDO::class);
+
+        $query = $db->query('CALL GetPemesanan()');
+        $results = $query->fetchAll(PDO::FETCH_ASSOC);
+        $response->getBody()->write(json_encode($results));
+
+        return $response->withHeader("Content-Type", "application/json");
+    });
+
+    // get tabel penumpang
+    $app->get('/penumpang', function(Request $request, Response $response) {
+        $db = $this->get(PDO::class);
+
+        $query = $db->query('CALL GetPenumpang()');
+        $results = $query->fetchAll(PDO::FETCH_ASSOC);
+        $response->getBody()->write(json_encode($results));
+
+        return $response->withHeader("Content-Type", "application/json");
+    });
+
+    // get tabel pesawat
+    $app->get('/pesawat', function(Request $request, Response $response) {
+        $db = $this->get(PDO::class);
+
+        $query = $db->query('CALL GetPesawat()');
+        $results = $query->fetchAll(PDO::FETCH_ASSOC);
+        $response->getBody()->write(json_encode($results));
+
+        return $response->withHeader("Content-Type", "application/json");
+    });
 };
--- app/settings.php
+++ app/settings.php
@@ -23,7 +23,7 @@ return function (ContainerBuilder $containerBuilder) {
                 ],
                 'db' => [
                     'host' => '127.0.0.1',
-                    'dbname' => 'mbd-slim-api',
+                    'dbname' => 'tiket_pesawat',
                     'user' => 'root',
                     'password' => '',
                 ]
