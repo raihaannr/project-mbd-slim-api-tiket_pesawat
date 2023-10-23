1d47a3a46462115e9bb2d76a76f8bc97aabf5824
Raihan Ramadhani <11211069@student.itk.ac.id>
2023-10-23 15:10:56 +0800

    get tabel by id

--- app/routes.php
+++ app/routes.php
@@ -21,6 +21,18 @@ return function (App $app) {
         return $response->withHeader("Content-Type", "application/json");
     });
 
+    //get tabel kelas by id
+    $app->get('/kelas/{idkelas}', function(Request $request, Response $response, $args){
+        $db = $this->get(PDO::class);
+
+        $query = $db->prepare('CALL GetKelasById(?)');
+        $query->execute([$args['idkelas']]);
+        $results = $query->fetchAll(PDO::FETCH_ASSOC);
+        $response->getBody()->write(json_encode($results[0]));
+
+        return $response->withHeader("Content-Type", "application/json");
+    });
+
     // get tabel pemesanan
     $app->get('/pemesanan', function(Request $request, Response $response) {
         $db = $this->get(PDO::class);
@@ -28,6 +40,18 @@ return function (App $app) {
         $query = $db->query('CALL GetPemesanan()');
         $results = $query->fetchAll(PDO::FETCH_ASSOC);
         $response->getBody()->write(json_encode($results));
+
+        return $response->withHeader("Content-Type", "application/json");
+    });
+
+    //get tabel pemesanan by id
+    $app->get('/pemesanan/{idpemesanan}', function(Request $request, Response $response, $args){
+        $db = $this->get(PDO::class);
+
+        $query = $db->prepare('CALL GetPemesananById(?)');
+        $query->execute([$args['idpemesanan']]);
+        $results = $query->fetchAll(PDO::FETCH_ASSOC);
+        $response->getBody()->write(json_encode($results[0]));
 
         return $response->withHeader("Content-Type", "application/json");
     });
@@ -43,6 +67,18 @@ return function (App $app) {
         return $response->withHeader("Content-Type", "application/json");
     });
 
+    //get tabel penumpang by id
+    $app->get('/penumpang/{idpenumpang}', function(Request $request, Response $response, $args){
+        $db = $this->get(PDO::class);
+
+        $query = $db->prepare('CALL GetPenumpangById(?)');
+        $query->execute([$args['idpenumpang']]);
+        $results = $query->fetchAll(PDO::FETCH_ASSOC);
+        $response->getBody()->write(json_encode($results[0]));
+
+        return $response->withHeader("Content-Type", "application/json");
+    });
+
     // get tabel pesawat
     $app->get('/pesawat', function(Request $request, Response $response) {
         $db = $this->get(PDO::class);
@@ -50,6 +86,18 @@ return function (App $app) {
         $query = $db->query('CALL GetPesawat()');
         $results = $query->fetchAll(PDO::FETCH_ASSOC);
         $response->getBody()->write(json_encode($results));
+
+        return $response->withHeader("Content-Type", "application/json");
+    });
+
+    //get tabel penumpang by id
+    $app->get('/pesawat/{idpenerbangan}', function(Request $request, Response $response, $args){
+        $db = $this->get(PDO::class);
+
+        $query = $db->prepare('CALL GetPesawatById(?);');
+        $query->execute([$args['idpenerbangan']]);
+        $results = $query->fetchAll(PDO::FETCH_ASSOC);
+        $response->getBody()->write(json_encode($results[0]));
 
         return $response->withHeader("Content-Type", "application/json");
     });
